868df8647cf2f110e393001df70ff1bfe674be92
Pandi92 <pandiyarajang92@gmail.com>
2024-03-06 12:08:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Register from './Register';
+import Login from './Login';
 
 function App() {
   return (
     <>
-    
+      <Register />
+      <Login />
+      <BrowserRouter>
+        {<Routes>
+          <Route path="/Register" element={<Register />}></Route>
+          <Route path="/Login" element={<Login />}></Route>
+
+        </Routes>}
+      </BrowserRouter>
     </>
   );
 }
